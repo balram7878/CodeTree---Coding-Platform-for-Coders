75120ce742a5c39a482ed9f45dc242b986957215
Balram Meena <bmeena97562@gmail.com>
2025-10-14 23:21:11 +0530

    integrate react-form

--- Frontend/Leetcode-Project/src/App.jsx
+++ Frontend/Leetcode-Project/src/App.jsx
@@ -1,7 +1,18 @@
+import { Routes, Route, BrowserRouter } from "react-router";
+import Login from "./component/Login";
+import Signup from "./component/Signup";
+import Homepage from "./component/Homepage";
+
 function App() {
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
